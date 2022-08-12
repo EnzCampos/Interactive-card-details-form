3baab9459cdc4eb9554a7e4a2992b7eec84f16a5
Enzo Campos <enzocampos@gmail.com>
2022-08-12 20:03:10 -0300

    Remade Styling Reponsiveness & Added Input handler

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ReactComponent as CardLogo } from "./card-logo.svg"
 import React from 'react'
 
 function App() {
@@ -8,44 +9,57 @@ function App() {
     cardExpDateYY: '00',
     cardCvc: '000',
   })
+  
+  function handleChange(event) {
+    setCardInfo(prevValue => ({
+      ...prevValue,
+      [event.target.name]: event.target.value
+    }))
+  }
 
   return (
     <div className="main-content">
       <div className='cards'>
-        <img src={require('./bg-card-front.png')} alt='card front' className="card front"/>
-        <h3 className="card-number">{cardInfo.cardNumber}</h3>
-        <h3 className="card-owner">{cardInfo.cardHolder}</h3>
-        <br/>
-        <img src={require('./bg-card-back.png')} alt='card front' className="card back"/>
-        <h3 className="card-cvc ">{cardInfo.cardCvc}</h3>
+        <div className='card front'>
+          <CardLogo className="card-logo"/>
+          <h3 className="card-number">{cardInfo.cardNumber}</h3>
+          <div className='flex'>
+            <h3 className="card-owner">{cardInfo.cardHolder}</h3>
+            <h3 className='exp-date'>{cardInfo.cardExpDateMM}/{cardInfo.cardExpDateYY}</h3>
+          </div>
+          <br/>
+        </div>
+        <div className='card back'>
+          <h3 className="cvc ">{cardInfo.cardCvc}</h3>
+        </div>
       </div>
       <div className="form-div">
         <form className="form center">
           <label>
             Card Holder Name
             <br/>
-            <input type="string" placeholder='eg. Enzo Campos' name='cardHolder'></input>
+            <input type="string" placeholder='eg. Enzo Campos' name='cardHolder' value={cardInfo.cardHolder} onChange={handleChange}></input>
           </label>
           <br/>
           <label>
             Card number
             <br/>
-            <input type="string" placeholder='eg. 1234 56789 1234 5678' name='cardNumber'></input>
+            <input type="string" placeholder='eg. 1234 56789 1234 5678' name='cardNumber' value={cardInfo.cardNumber} onChange={handleChange}></input>
           </label>
           <br/>
           <div className='card-date-cvc'>
-            <label>
-              EXP. DATE (MM/YY)
+            <label className='exp-date'>
+              Exp. Date
               <br/>
-              <div className='exp-date'>
-                <input type="number" placeholder='MM' name='cardExpDateMM' min='01' max='12' className="card-date"></input>
-                <input type="number" placeholder='YY' name='cardExpDateYY' min='2022' className="card-date yy"></input>
+              <div>
+                <input type="number" placeholder='MM' name='cardExpDateMM' min='01' max='12' className="card-date mm" value={cardInfo.cardExpDateMM} onChange={handleChange}></input>
+                <input type="number" placeholder='YY' name='cardExpDateYY' min='2022' className="card-date yy" value={cardInfo.cardExpDateYY} onChange={handleChange}></input>
               </div>
             </label>
-            <label className='cvc'>
+            <label className='cvc-label'>
               CVC
               <br/>
-              <input type="number" placeholder='e.g. 123' name='cardCvc' min='0' className='card-cvc'></input>
+              <input type="number" placeholder='e.g. 123' name='cardCvc' min='0' className='card-cvc' value={cardInfo.cardCvc} onChange={handleChange}></input>
             </label>
           </div>
           <label>
